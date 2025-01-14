4ea0a42ce9fc836392be5706f7015f73fbaa4872
Kadeejath Salaha <codestudy475@gmail.com>
2025-01-15 04:25:22 +0530

    Initialize backend with database configuration and user model

---
+++ Chapter 1/backend/models/user.js
@@ -0,0 +1,13 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    name:{type:DataTypes.STRING, allowNull:false},
+    email:{type:DataTypes.STRING, allowNull:false, unique:true},
+    password:{type:DataTypes.STRING, allowNull:false},
+},{
+    tableName:'users',
+    timestamps:true,
+});
+
+module.exports = User;
